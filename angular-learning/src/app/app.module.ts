c0f80c00594056b63720e0523ca70e1c8d95e001
Jaypal Sodha <jaypalsodha999@gmail.com>
2019-12-18 22:02:53 +0530

    app-module-component-declaration

--- angular-learning/src/app/app.module.ts
+++ angular-learning/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// angular uses component to create the we pages.
+// angular uses modules to bundle all the components
+// all the features will be defined in this module. 
+// it defines our entire application
+//
