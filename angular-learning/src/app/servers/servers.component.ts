8f4cfa446339ea4d20a11dbbf258f326a324168f
Jaypal Sodha <jaypal.sodha@highq.com>
2019-12-19 15:42:39 +0530

    structural-directive

--- angular-learning/src/app/servers/servers.component.ts
+++ angular-learning/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverName :string="test server";
   serverCreationStatus:string ="No server was created!";
+  serverCreated:boolean=false;
   constructor() {
     setTimeout((
     ) => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
   }
   
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = "Server was created! Server name is :- "+ this.serverName;
   }
 }
