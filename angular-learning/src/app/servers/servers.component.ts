87b65a6cd7c25b5aa32dae2b47c0629c24f79136
Jaypal Sodha <jaypal.sodha@highq.com>
2019-12-19 15:25:44 +0530

    all-forms-data-binding

--- angular-learning/src/app/servers/servers.component.ts
+++ angular-learning/src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer: boolean = false;
-  serverName :string="";
+  serverName :string="test server";
   serverCreationStatus:string ="No server was created!";
   constructor() {
     setTimeout((
@@ -25,6 +25,6 @@ export class ServersComponent implements OnInit {
   }
   
   onCreateServer(){
-    this.serverCreationStatus = "Server was created!"
+    this.serverCreationStatus = "Server was created! Server name is :- "+ this.serverName;
   }
 }
