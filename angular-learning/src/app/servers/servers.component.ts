c93d8e51c2b8c7800013e0b846b26eb3c7945349
Jaypal Sodha <jaypal.sodha@highq.com>
2019-12-19 12:33:41 +0530

    event-binding

--- angular-learning/src/app/servers/servers.component.ts
+++ angular-learning/src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer: boolean = false;
-
+  serverCreationStatus:string ="No server was created!";
   constructor() {
     setTimeout((
     ) => {
@@ -21,6 +21,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+  
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created!"
   }
 
 
